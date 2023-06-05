421c2db8e862ff21a8e9c9057553de15b8bbd768
Karthikeyan N <109551096+Karthikoffl@users.noreply.github.com>
2023-06-05 10:31:39 +0530

    unfinished commit

---
+++ src/common/form.js
@@ -0,0 +1,29 @@
+module.exports = {
+  head1: {
+    fontSize: 30,
+    color: "black",
+    textAlign: "center",
+  },
+  head2: {
+    fontSize: 20,
+    color: "black",
+    textAlign: "center",
+  },
+  formgroup: {
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    marginVertical: 10,
+  },
+  label: {
+    fontSize: 17,
+    color: "#000",
+    marginLeft: 10,
+    marginBottom: 5,
+  },
+  input: {
+    backgroundColor: "#FFB0CC",
+    borderRadius: 20,
+    padding: 13,
+  },
+};
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, TextInput } from "react-native";
 import React from "react";
 import pattern from "../../assets/pattern.png";
 import logo from "../../assets/mainlogo.png";
+import { formgroup, head1, head2, input, label } from "../common/form";
 
 const Login = () => {
   return (
@@ -9,11 +10,22 @@ const Login = () => {
       <Image style={styles.patternbg} source={pattern} />
       <View style={styles.container1}>
         <View style={styles.s1}>
-          <Image source={logo} style={styles.logo} />
+          {/* <Image source={logo} style={styles.logo} /> */}
           <Text style={styles.h1}>Hi</Text>
-          <Text style={styles.small1}>Hi</Text>
+          <Text style={styles.small1}>User</Text>
         </View>
-        <View style={styles.s2}></View>
+        <View style={styles.s2}>
+          <Text style={head1}>Login</Text>
+          <Text style={head2}>Sign In To Continue</Text>
+          <View style={formgroup}>
+            <Text style={label}>Email</Text>
+            <TextInput style={input} />
+          </View>
+          <View style={formgroup}>
+            <Text style={label}>Password</Text>
+            <TextInput style={input} />
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -40,6 +52,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     height: "100%",
   },
+  s1: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "50%",
+  },
   small1: {
     color: "#fff",
     fontSize: 17,
@@ -47,5 +65,31 @@ const styles = StyleSheet.create({
   h1: {
     fontSize: 30,
     color: "#fff",
+  },
+  s2: {
+    display: "flex",
+    backgroundColor: "#fff",
+    width: "100%",
+    height: "50%",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    padding: 20,
+  },
+  formgroup: {
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    marginVertical: 10,
+  },
+  label: {
+    fontSize: 17,
+    color: "#000",
+    marginLeft: 10,
+    marginBottom: 5,
+  },
+  input: {
+    backgroundColor: "#FFB0CC",
+    borderRadius: 20,
+    padding: 10,
   },
 });
